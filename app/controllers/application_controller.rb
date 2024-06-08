7ec3592c937398420e46ce590eef490cd0471723
Tim Standen <timmstanden@gmail.com>
2024-06-07 23:28:29 -0700

    Change welcome to Welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  def welcome 
+  def Welcome 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "application#welcome"
+  root "application#Welcome"
 end
