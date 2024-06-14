4e3f9e67d540a2c603d5a119b4bbdbae801a7c52
Tim Standen <timmstanden@gmail.com>
2024-06-13 21:33:43 -0700

    Add view transitions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+addEventListener("turbo:before-frame-render", (event) => {
+  if (document.startViewTransition) {
+    const originalRender = event.detail.render;
+    event.detail.render = (currentElement, newElement) => {
+      document.startViewTransition(() => originalRender(currentElement, newElement));
+    };
+  }
+});
